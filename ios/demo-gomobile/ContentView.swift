1b12ac360314c16b279768dca537d71dfc2b4f0f
BH_Lin <binghuanlin@gmail.com>
2021-06-16 12:49:56 +0800

    check-in ios project

---
+++ ios/demo-gomobile/ContentView.swift
@@ -0,0 +1,32 @@
+//
+//  ContentView.swift
+//  demo-gomobile
+//
+//  Created by BH_Lin on 2021/6/16.
+//
+
+import SwiftUI
+import Hello
+
+struct ContentView: View {
+    
+    func callMyFunction() {
+        HelloGreetings("BH_Lin");
+        HelloGetVersion()
+    }
+    
+    var body: some View {
+        VStack() {
+            Text("Version: \(HelloGetVersion())")
+            Button(action: {}) {
+                Text("PRESS ME TO CALL FUNC")
+            }
+        }
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ ios/demo-gomobile/demo_gomobileApp.swift
@@ -0,0 +1,17 @@
+//
+//  demo_gomobileApp.swift
+//  demo-gomobile
+//
+//  Created by BH_Lin on 2021/6/16.
+//
+
+import SwiftUI
+
+@main
+struct demo_gomobileApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
