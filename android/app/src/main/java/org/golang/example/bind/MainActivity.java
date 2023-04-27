06eca101f5456019495710519bf0b1d6335f818c
BH_Lin <binghuanlin@gmail.com>
2023-04-28 01:35:40 +0800

    Upgrade gradle version for Android.

--- android/app/src/main/java/org/golang/example/bind/MainActivity.java
+++ android/app/src/main/java/org/golang/example/bind/MainActivity.java
@@ -14,13 +14,11 @@ import hello.Hello;
 
 public class MainActivity extends Activity {
 
-    private TextView mTextView;
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        mTextView = (TextView) findViewById(R.id.mytextview);
+        TextView mTextView = findViewById(R.id.mytextview);
 
         // Call Go function.
         String greetings = Hello.greetings("Android and Gopher");
